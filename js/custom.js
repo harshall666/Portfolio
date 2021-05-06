012cc2fc5078c8effd9f06aead8703f682d317e4
BuildTools <unconfigured@null.spigotmc.org>
2021-05-06 20:43:49 +0530

    Major changes

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,18 @@
 // PRELOADER
 $(window).load(function(){
-    $('.preloader').delay(500).fadeOut("slow"); // set duration in brackets    
+    $('.preloader').delay(500).fadeOut("slow");   
 });
 
 // HOME BACKGROUND SLIDESHOW
 $(function(){
     jQuery(document).ready(function() {
-		$('body').backstretch([
-	 		 "images/bg-slide-1.jpg", 
-	 		 "images/bg-slide-2.jpg",
-			 "images/bg-slide-3.jpg"
-	 			], 	{duration: 3200, fade: 1300});
+		$("body").backstretch(
+      [
+        "https://source.unsplash.com/1920x1280/?html",
+        "https://source.unsplash.com/1920x1280/?programmer",
+        "https://source.unsplash.com/1920x1280/?website",
+      ],
+      { duration: 3200, fade: 1300 }
+    );
 		});
 })
